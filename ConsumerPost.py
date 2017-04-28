7cede9e62bb172bda67599b8f19564141fc0dc0f
Dauzy <daniel_6287@hotmail.com>
2017-04-28 13:14:52 -0500

    Se limpio el codigo

--- ConsumerPost.py
+++ ConsumerPost.py
@@ -24,36 +24,25 @@ class ConsumerPost:
 		
 		for msg in self.consumer:
 			
-			#cadena = str(msg.value)
-			#print(">>>>>>>>>completo")
-			#print(str(msg.value.decode('utf8')))
 			sppls = str(msg.value.decode('utf8')).split(" ")
 			
 			if sppls[0] == '::1' or re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",sppls[0]):
 				
-				print(">>>>>>>>>>>APACHE")
-				#print("dir: "+sppls[0])
 				chi = sppls[0]
 				space1 = sppls[1]
 				caun = sppls[2]
 				fechita = sppls[3]
 				fechita = fechita.replace("[", "")
-				#print(">>>>>>>>"+fechita)
 				time_struct1, parse_status1 = p.parse(fechita)
-				#print(time_struct1)
-				print(">>>>>"+fechita)
 				cqtd = datetime(*time_struct1[:6])
 				cqtx_es = " "
 				cqtx = (str(cqtx_es.join(sppls[5:8])))
-				# print(sppls)
 				pssc = sppls[8]
 				pscl = sppls[9]
-				print(chi +" "+ space1+" "+caun+" "+ str(cqtd)+" >>"+cqtx +" "+pssc+" "+pscl)
 				self.postsql.saveDataApache('dummydb','postgres','secretpass', chi, space1, caun, cqtd, cqtx, pssc, pscl)
+			
 			else:
 				
-				#cadena = str(msg.value)
-				#print(">>>>>>>>>>>MESSAGESS")
 				time_struct, parse_status = p.parse(str(msg.value))
 				fecha = datetime(*time_struct[:6])
 				fecha = str(fecha)
@@ -61,11 +50,7 @@ class ConsumerPost:
 				dirhost =  cadena.split(" ")[3]
 				codeLog = cadena.split(" ")[4]
 				logMessage = cadena.split(" ")[5:]
-				#print(fecha)
-				#print(dirhost)
 				es = " "
-				#print(codeLog)
-				#print(es.join(logMessage))
 				self.postsql.saveData('dummydb','postgres','secretpass', fecha, dirhost, codeLog,str(es.join(logMessage)))
 
 
--- PyPost.py
+++ PyPost.py
@@ -11,14 +11,11 @@ class PyPost:
 		global conn 
 
 		try:
-			#print(dbname+"  "+usrdb+" "+pswd+" "+self.url+" "+self.portToPost)
 			conn = psycopg2.connect(database=dbname, user = usrdb, password = pswd, host = self.url, port = self.portToPost)
 			print("Opened database successfully")
 		except BaseException as e:
 			print("Can't Opened database.")
-
 		
-
 		try:
 			query = "INSERT INTO DUMMYLOGS VALUES (default, %s, %s, %s, %s)"
 			data = (logs_date,logs_hots, logs_code, logs_message)
@@ -26,13 +23,9 @@ class PyPost:
 			cur.execute(query,data)
 			conn.commit()
 			print("Data is write!")
-			
 		except BaseException as e: 
 			print("can't connecto to db: ERROR ", e)
 
-#		rows = cur.fetchall()
-#		for row in rows:
-#			print("   ",row)
 
 	def saveDataApache(self, dbname, usrdb, pswd, logs_chi, logs_space, logs_caun, logs_cqtd, logs_cqtx, logs_pssc, pscl):
 		global conn 
@@ -43,6 +36,7 @@ class PyPost:
 		except BaseException as e:
 			print("Can't Opened database.")
 
+			
 		try:
 			query = "INSERT INTO APACHEDUMMYLOGS2 VALUES (default, %s, %s,%s, %s, %s, %s, %s)"
 			data = (logs_chi, logs_space,logs_caun, logs_cqtd, logs_cqtx, logs_pssc, pscl)
@@ -50,8 +44,6 @@ class PyPost:
 			cur.execute(query,data)
 			conn.commit()
 			print("APACHE: Data is write!")
-			
 		except BaseException as e: 
+			
 			print("can't connecto to db: ERROR ", e)
-#prueba = PyPost('127.0.0.1','5432')
-#prueba.saveData('dummydb','postgres','secretpass', '2017-04-26 16:03:50','localhost','fprintd: ** Message: entering main loop')
